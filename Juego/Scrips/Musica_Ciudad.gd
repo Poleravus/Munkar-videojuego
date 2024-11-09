4df4c7d1ed836e15355df0f0706d622b8f4156b3
DiegoEspinozaLoreto <diego.espinoza8841@alumnos.udg.mx>
2024-11-08 20:47:29 -0600

    Trasncicion a  escalera

--- Juego/Scrips/Musica_Ciudad.gd
+++ Juego/Scrips/Musica_Ciudad.gd
@@ -3,3 +3,8 @@ extends StaticBody2D
 @onready var player = $LOOP1
 func _ready():
 	player.play()
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("isaac"):
+		get_tree().change_scene_to_file("res://Escenas/Escalera.tscn")
